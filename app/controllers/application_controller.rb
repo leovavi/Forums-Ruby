ddddc76c626a048564b83b780110877b8e3d711a
Leonardo Vásquez <leonardovavi@unitec.edu>
2018-02-08 12:10:35 -0600

    Forums

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permited_params, if: :devise_controller?
+
+  protected
+  	def configure_permited_params
+  		devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  	end
+
+end
